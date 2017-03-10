de906da508e730fbc6b1142896516d070329f84a
simon <tian_xiqin@163.com>
2017-03-10 14:06:14 +0800

    Update mysqli_conn.php

--- libs/mysqli_conn.php
+++ libs/mysqli_conn.php
@@ -10,7 +10,7 @@
 		public $conn;
 
 		function __construct(){
-			$this->conn=new mysqli("localhost","root","txq19940407.","personal") or die(mysqli_connect_error());
+			$this->conn=new mysqli("localhost","","","personal") or die(mysqli_connect_error());
 			if($this->conn ->connect_errno){
 				die("连接错误:".$this->conn ->connect_error);
 				exit();
