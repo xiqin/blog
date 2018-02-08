424b78f9ab7f0f53a77da52e470117fc5a3725ce
simon <tian_xiqin@163.com>
2018-02-08 10:01:57 +0800

    Update mysqli_conn.php

--- libs/mysqli_conn.php
+++ libs/mysqli_conn.php
@@ -1,11 +1,4 @@
 <?php
-
-// 	$conn=new mysqli("localhost","root","txq19940407","personal") or die(mysqli_connect_error());
-// 	if($conn -> connect_errno){
-// 		die("连接错误:".$this->conn -> connect_error);
-// 		exit();
-// 	}
-//	$conn -> set_charset('utf8');
 	class Conn{
 		public $conn;
 
